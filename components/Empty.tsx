e82f6a82c544967450ddc2ae897c5b1343fdaf17
siyeol97 <leesi2830@naver.com>
2024-10-14 13:57:13 +0900

    💄 UI : add images

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -1,9 +1,13 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 import React from 'react';
 
 export default function Empty() {
   return (
     <View style={styles.wrapper}>
+      <Image
+        source={require('../assets/images/young_and_happy.png')}
+        style={styles.image}
+      />
       <Text style={styles.description}>할 일이 없습니다잉~</Text>
     </View>
   );
@@ -18,5 +22,10 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 24,
     color: '#9e9e9e',
+  },
+  image: {
+    width: 240,
+    height: 179,
+    marginBottom: 16,
   },
 });
